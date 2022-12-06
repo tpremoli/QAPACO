0095900fb325a629ac474d59f08c1801ee72fab8
tpremoli <tomas@premoli.org>
2022-12-06 16:32:45 +0000

    Added quick test to see if pheromone works (it does)

--- world.py
+++ world.py
@@ -138,7 +138,18 @@ class World:
 if __name__  == "__main__":
     w = World("Uni50a.dat", m=100, e=0.9)
     
-    for x in range(1000):
+    a = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49]]
+    c, ba = w.calc_fitnesses(a)
+    for _ in range(500):
+        w.apply_pheromones(a,c)
+        w.evaporate_pheromones()
+    
+    a = w.generate_ant_paths()
+    c,ba = w.calc_fitnesses(a)
+
+
+    
+    for x in range(0):
         a = w.generate_ant_paths()
         c,ba = w.calc_fitnesses(a)
         w.apply_pheromones(a,c)
