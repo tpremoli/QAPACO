913f87033667e670f3a8c976ee36f9461aa42557
tpremoli <tomas@premoli.org>
2022-12-15 02:01:09 +0000

    Created multithreaded (pooled) processing

--- world.py
+++ world.py
@@ -1,6 +1,7 @@
 from random import SystemRandom
 import matplotlib.pyplot as plt
 import numpy.random as npr
+from multiprocessing import Pool
 
 class World:
     def __init__(self, filename, m=100, e=0.5, print_data=True):
@@ -259,18 +260,58 @@ def runtest(w):
     assert a[0] == ba
     assert c[0] == 5941988
 
-if __name__  == "__main__":
-    w = World("Uni50a.dat", m=10, e=0.9)
+def single_aco(m, e, i, max_iter, print_data):
+    """This method runs the ACO with the specified parameters and plots them. This will usually be ran as a subprocess (do multiple attempts @once)
 
-
-    # runtest(w)
-    
-    for x in range(1000):
+    Args:
+        e (float): e val for world, evaporation rate
+        m (int): m val for world, ant count
+        max_iter (int, optional): Max interations of the process. Defaults to 1000.
+        print_data (bool, optional): Whether to print each step's data. Defaults to False.
+    """
+    print("launching {}".format(i+1))
+    w = World("Uni50a.dat", m=m, e=e, print_data=print_data)
+    bestant_cost = 10000000000000000000000000000000000000000000000
+    bestant = []
+    for x in range(max_iter):
         a = w.generate_ant_paths()
-        c,ba, bai = w.calc_fitnesses(a, x+1)
+        c, new_bestant, new_bestant_cost = w.calc_fitnesses(a, x+1, print_data=print_data)
         w.apply_pheromones(a,c)
         w.evaporate_pheromones()
-    w.plt_fitnesses(num = 0)
-    plt.savefig("hello.png")
-        
+        if new_bestant_cost < bestant_cost:
+            bestant = new_bestant
+            bestant_cost = new_bestant_cost
+
+    avgant_cost = w.get_total_avg()
+    w.plt_fitnesses(num=i+1)
+    print("\tattempt {} best ant cost: {}\n\tavg ant cost: {}\n\tant:{}".format(i+1,bestant_cost,avgant_cost,bestant))
+    plt.show()
+    
+def run_process(no_attempts, m, e, max_iter=10_000, print_data=False):
+    """Creates multiprocesses ACO according to no_attempts
+    
+    Args:
+        no_attempts (int): how many times to run the ACO with the parameters
+        e (float): e val for world, evaporation rate
+        m (int): m val for world, ant count
+        max_iter (int, optional): Max interations of the process. Defaults to 1000.
+        print_data (bool, optional): Whether to print each step's data. Defaults to False.
+    """
+    
+    print("m: {}, e: {}".format(m,e))
+    
+    # We create pools with processes as no attempts (10,000 iterations is a lot, so better to run experiments concurrently)
+    pool = Pool(processes=no_attempts)
+    inputs = []
+    for i in range(no_attempts):
+        inputs.append((m,e,i,max_iter,print_data))
+    
+    # We use tuples for arguments
+    pool.starmap(single_aco,inputs)
+    
+if __name__  == "__main__":
+    
+    run_process(no_attempts=5,m=10, e=0.9)
+
+    
     
